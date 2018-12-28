46ec2a4b813e34e7bcee60ef3baed652201ce248
Edvardas <edvard.salc@gmail.com>
2018-12-28 13:26:30 +0200

    db add

--- ConsoleApp001/Program.cs
+++ ConsoleApp001/Program.cs
@@ -1,3 +1,5 @@
+using System.Data.Entity;
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,15 +12,62 @@ namespace ConsoleApp001
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            using (var db = new BloggingContext("MainDB"))
+            {
+                // Create and save a new Blog
+                Console.Write("Enter a name for a new Blog: ");
+                var name = Console.ReadLine();
 
+                var blog = new Blog { Name = name };
+                db.Blogs.Add(blog);
+                db.SaveChanges();
 
+                // Display all Blogs from the database
+                var query = from b in db.Blogs
+                            orderby b.Name
+                            select b;
 
+                Console.WriteLine("All blogs in the database:");
+                foreach (var item in query)
+                {
+                    Console.WriteLine(item.Name);
+                }
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey();
+            }
         }
+
+        public class Blog
+        {
+            public int BlogId { get; set; }
+            public string Name { get; set; }
+
+            public virtual List<Post> Posts { get; set; }
+        }
+
+        public class Post
+        {
+            public int PostId { get; set; }
+            public string Title { get; set; }
+            public string Content { get; set; }
+
+            public int BlogId { get; set; }
+            public virtual Blog Blog { get; set; }
+        }
+
+        public class BloggingContext : DbContext
+        {
+            public BloggingContext(string connString) : base(connString)
+            {
+           
+            }
+        
+
+
+            public DbSet<Blog> Blogs { get; set; }
+            public DbSet<Post> Posts { get; set; }
+        }
+
     }
 }
